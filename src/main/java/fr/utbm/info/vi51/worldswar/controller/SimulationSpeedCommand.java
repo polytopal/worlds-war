4998452ad35d7f8cf68bb363bd1ea5c692a7ae8b
Polytopal <pierrick@estvideo.fr>
2016-04-14 21:56:03 +0200

    Remove ControllerCommandType enum as it did not do anything useful.

--- src/main/java/fr/utbm/info/vi51/worldswar/controller/SimulationSpeedCommand.java
+++ src/main/java/fr/utbm/info/vi51/worldswar/controller/SimulationSpeedCommand.java
@@ -1,19 +1,16 @@
 package fr.utbm.info.vi51.worldswar.controller;
 
-import static fr.utbm.info.vi51.worldswar.controller.ControllerCommandType.SIMULATION_SPEED;
-
 import fr.utbm.info.vi51.worldswar.simulator.SimulationSpeed;
 
 /**
  * {@link ControllerCommand} requesting a simulation speed
  * 
  */
-public class SimulationSpeedCommand extends ControllerCommand {
+public class SimulationSpeedCommand {
 
 	private SimulationSpeed speed;
 
 	public SimulationSpeedCommand(SimulationSpeed speed) {
-		super(SIMULATION_SPEED);
 		this.speed = speed;
 	}
 
