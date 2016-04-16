3287d6a3687c8f6fed12d67d47f23bdeb75608ad
Leo Roggy <leo.roggy@utbm.fr>
2016-04-16 12:30:21 +0200

    Collection grid changed to Grid

--- src/main/java/fr/utbm/info/vi51/worldswar/utils/Grid.java
+++ src/main/java/fr/utbm/info/vi51/worldswar/utils/Grid.java
@@ -1,19 +1,19 @@
 package fr.utbm.info.vi51.worldswar.utils;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 
 /**
  * @author Leo
  *
  * @param <T>
  * 
- *            This Class is a generic that contains a matrix of collection of
- *            type T.
+ *            This Class is a generic that contains a matrix of objects of type
+ *            T.
  */
-public class CollectionGrid<T> {
+public class Grid<T> {
 
-	private final ArrayList<ArrayList<ArrayList<T>>> grid;
+	private final List<List<T>> grid;
 
 	private final int xOffset;
 	private final int yOffset;
@@ -28,10 +28,10 @@ public class CollectionGrid<T> {
 	 * @param yMax
 	 * 
 	 *            Build collection grid with the extreme points, each cell
-	 *            contains an empty collection. Each coordinate can be positive
-	 *            or negative.
+	 *            contains a null object. Each coordinate can be positive or
+	 *            negative.
 	 */
-	public CollectionGrid(int xMin, int xMax, int yMin, int yMax) {
+	public Grid(int xMin, int xMax, int yMin, int yMax) {
 
 		if (xMin >= xMax) {
 			throw new IllegalArgumentException("xMax have to be greater than xMin"); //$NON-NLS-1$
@@ -50,11 +50,9 @@ public class CollectionGrid<T> {
 		this.grid = new ArrayList<>(this.height);
 		for (int y = yMin; y <= yMax; y++) {
 			// build each line
-			final ArrayList<ArrayList<T>> line = new ArrayList<>(this.width);
-			for (int x = xMin; x <= xMax; x++) {
-				// build the list of each cell
-				final ArrayList<T> cellList = new ArrayList<>();
-				line.add(cellList);
+			final List<T> line = new ArrayList<>(this.width);
+			for (int x = yMin; x <= xMax; x++) {
+				line.add(null);
 			}
 			this.grid.add(line);
 		}
@@ -106,39 +104,28 @@ public class CollectionGrid<T> {
 	/**
 	 * @param x
 	 * @param y
-	 * @return the collection at the position (x,y)
+	 * @return the value at the position (x,y)
 	 */
-	public Collection<T> get(int x, int y) {
+	public T get(int x, int y) {
 		return this.grid.get(y + this.yOffset).get(x + this.xOffset);
 	}
 
 	/**
-	 * Set the collection at the position (x,y)
-	 * 
-	 * @param x
-	 * @param y
-	 * @param collection
-	 */
-	public void set(int x, int y, Collection<T> collection) {
-		this.grid.get(y + this.yOffset).set(x + this.xOffset, new ArrayList<>(collection));
-	}
-
-	/**
-	 * Add an element to the collection at the position (x,y)
+	 * Set the value at the position (x,y)
 	 * 
 	 * @param x
 	 * @param y
 	 * @param value
 	 */
-	public void add(int x, int y, T value) {
-		this.grid.get(y + this.yOffset).get(x + this.xOffset).add(value);
+	public void set(int x, int y, T value) {
+		this.grid.get(y + this.yOffset).set(x + this.xOffset, value);
 	}
 
 	@Override
 	public String toString() {
 		final StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append("["); //$NON-NLS-1$
-		for (final ArrayList<ArrayList<T>> line : this.grid) {
+		for (final List<T> line : this.grid) {
 			stringBuilder.append(line + "\n"); //$NON-NLS-1$
 		}
 		stringBuilder.append("]"); //$NON-NLS-1$
