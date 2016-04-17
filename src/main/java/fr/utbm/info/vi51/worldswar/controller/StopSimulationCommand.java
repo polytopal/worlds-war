a0bbdfe22f675676ef8514137afa615e3e1e3c6b
Eddh <jegghel@gmail.com>
2016-04-17 16:25:03 +0200

    newSimulation and StopSimulation implemented

---
+++ src/main/java/fr/utbm/info/vi51/worldswar/controller/StopSimulationCommand.java
@@ -0,0 +1,9 @@
+package fr.utbm.info.vi51.worldswar.controller;
+
+/**
+ * {@link ControllerCommand} requesting a stop in the simulation
+ * 
+ */
+public class StopSimulationCommand {
+	// nothing? to be determined
+}
