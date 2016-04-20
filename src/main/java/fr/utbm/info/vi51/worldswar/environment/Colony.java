eba8b76c27ab23f4c698e00b92de8f9dcaaec6e7
Polytopal <pierrick@estvideo.fr>
2016-04-20 20:54:47 +0000

    Colony class simplification

--- src/main/java/fr/utbm/info/vi51/worldswar/environment/Colony.java
+++ src/main/java/fr/utbm/info/vi51/worldswar/environment/Colony.java
@@ -7,24 +7,15 @@ package fr.utbm.info.vi51.worldswar.environment;
  */
 public class Colony {
 
-	/**
-	 * Unique identifier of the colony in the environment.
-	 */
-	private int id;
-
 	private Breed breed;
 
 	/**
-	 * Create a new Colony
+	 * Create a new ant Colony
 	 * 
-	 * @param id
-	 *            an integer that must be a unique identifier of the colony for
-	 *            the simulation
 	 * @param breed
-	 *            the {@link Breed} of the ants of the colony
+	 *            the {@link Breed} of the ants of this colony
 	 */
-	private Colony(int id, Breed breed) {
-		this.id = id;
+	private Colony(Breed breed) {
 		this.breed = breed;
 	}
 
@@ -34,28 +25,6 @@ public class Colony {
 
 	public void setBreed(Breed breed) {
 		this.breed = breed;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + this.id;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (this.getClass() != obj.getClass())
-			return false;
-		Colony other = (Colony) obj;
-		if (this.id != other.id)
-			return false;
-		return true;
 	}
 
 }
