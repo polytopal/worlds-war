092ad9ab631f68b4d6eb19e89fe12e372cf078ea
Leo Roggy <leo.roggy@utbm.fr>
2016-05-03 14:43:01 +0200

    bug fix

--- src/main/sarl/fr/utbm/info/vi51/worldswar/environment/Breed.java
+++ src/main/sarl/fr/utbm/info/vi51/worldswar/environment/Breed.java
@@ -5,7 +5,7 @@ package fr.utbm.info.vi51.worldswar.environment;
  *
  */
 public enum Breed {
-	DARK_ANTS(Messages.getString("Breed.darkAnts")), RED_ANTS(Messages.getString("Breed.redAnts")); //$NON-NLS-1$ //$NON-NLS-2$
+	DARK_ANTS("Breed.darkAnts"), RED_ANTS("Breed.redAnts"); //$NON-NLS-1$ //$NON-NLS-2$
 
 	private String stringRepresentation;
 
@@ -15,6 +15,6 @@ public enum Breed {
 
 	@Override
 	public String toString() {
-		return stringRepresentation;
+		return Messages.getString(this.stringRepresentation);
 	}
 }
