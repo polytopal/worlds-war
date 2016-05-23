58b7a122f56dfc41877c9e9d8dae3457fa13c085
Polytopal <pierrick@estvideo.fr>
2016-05-23 12:38:43 +0200

    Add javadoc to GathererStrategicBehaviour

--- src/main/sarl/fr/utbm/info/vi51/worldswar/agent/behaviour/strategic/GathererStrategicBehaviour.java
+++ src/main/sarl/fr/utbm/info/vi51/worldswar/agent/behaviour/strategic/GathererStrategicBehaviour.java
@@ -6,10 +6,17 @@ import fr.utbm.info.vi51.worldswar.agent.behaviour.tactical.AntTacticalBehaviour
 import fr.utbm.info.vi51.worldswar.environment.influence.Influence;
 import fr.utbm.info.vi51.worldswar.perception.AntPerception;
 
+/**
+ * Implementation of a {@link AntStrategicBehaviour} for gatherers
+ *
+ */
 public class GathererStrategicBehaviour implements AntStrategicBehaviour {
 
 	private final AntTacticalBehaviour tacticalBehaviour;
 
+	/**
+	 * @param tacticalBehaviour
+	 */
 	public GathererStrategicBehaviour(AntTacticalBehaviour tacticalBehaviour) {
 		this.tacticalBehaviour = tacticalBehaviour;
 	}
