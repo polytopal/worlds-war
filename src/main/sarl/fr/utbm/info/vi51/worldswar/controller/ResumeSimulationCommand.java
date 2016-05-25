f33a752f48f5464bde6e09a1cda608d5c2cbcaad
Polytopal <pierrick@estvideo.fr>
2016-05-25 09:35:21 +0200

    Warnings clean up in controller package

--- src/main/sarl/fr/utbm/info/vi51/worldswar/controller/ResumeSimulationCommand.java
+++ src/main/sarl/fr/utbm/info/vi51/worldswar/controller/ResumeSimulationCommand.java
@@ -1,7 +1,7 @@
 package fr.utbm.info.vi51.worldswar.controller;
 
 /**
- * {@link ControllerCommand} requesting the simulation to stop the pause.
+ * {@link ControllerCommandEvent} requesting the simulation to stop the pause.
  * 
  */
 public class ResumeSimulationCommand {
