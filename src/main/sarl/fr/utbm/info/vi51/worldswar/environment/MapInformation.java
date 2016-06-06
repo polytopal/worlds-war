866471e472586b0ee4feae051260f092696f66b6
Eddh <jegghel@gmail.com>
2016-06-06 09:57:48 +0200

    colonyList in mapInformation

--- src/main/sarl/fr/utbm/info/vi51/worldswar/environment/MapInformation.java
+++ src/main/sarl/fr/utbm/info/vi51/worldswar/environment/MapInformation.java
@@ -1,5 +1,7 @@
 package fr.utbm.info.vi51.worldswar.environment;
 
+import java.util.List;
+
 /**
  * MapInformation is a class that is meant to contain various information
  * related to the simulation map. The user can access this information thanks
@@ -8,13 +10,16 @@ package fr.utbm.info.vi51.worldswar.environment;
 public class MapInformation {
 	
 	private final int noiseSeed;
+	private final List<Colony> colonyMap;
 	
 	/**
 	 * Constructor
 	 * @param noiseSeed
+	 * @param colonyMap 
 	 */
-	public MapInformation(int noiseSeed){
+	public MapInformation(int noiseSeed, List<Colony> colonyMap){
 		this.noiseSeed = noiseSeed;
+		this.colonyMap = colonyMap;
 	}
 	
 	/**
@@ -22,6 +27,13 @@ public class MapInformation {
 	 */
 	public int getNoiseSeed(){
 		return this.noiseSeed;
+	}
+	
+	/**
+	 * @return colonyMap a collection of the colonies in the simulation
+	 */
+	public List<Colony> getColonyMap() {
+		return this.colonyMap;
 	}
 
 }
